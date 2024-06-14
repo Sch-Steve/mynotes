8fd0a58e27a794be5d4503544ae82c61a141d7ef
Sch-Steve <steveschoder@gmail.com>
2024-06-14 14:20:50 +0200

    Adding notes count to NotesPage.js

--- src/pages/NotesPage.js
+++ src/pages/NotesPage.js
@@ -4,7 +4,12 @@ import ListItem from "../components/ListItem";
 
 const NotesPage = () => {
   return (
-    <div>
+    <div className="notes">
+
+      <div className="notes-header">
+        <h2 className="notes-title">&#9782;Notes</h2>
+        <p className="notes-count">{notes.length}</p>
+      </div>
       {notes.map((note, index) => (
         <ListItem key={index} note={note} />
       ))}
