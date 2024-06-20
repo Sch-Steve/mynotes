bdef76f0c28b1bc3b42815c3d155dd2aa4f9d0ec
Sch-Steve <101710869+Sch-Steve@users.noreply.github.com>
2024-06-20 18:23:05 +0200

    Before database utilisation (#1)
    * Adding ListItem.js to display data.js
    
    * Added basic routing to App.js
    
    * Adding <Link> to notes in listItem.js
    
    * added css from dennis repo
    
    * Applied dark theme and basic container + app styling
    
    * Adding notes count to NotesPage.js
    
    * Adding back arrow in NotePage.js
    
    * Adding Header style and changing text in Header.js component
    
    * Remved redundant notes text in NotePage.js
    
    * Pushing arrow-left.svg to remote repo
    
    * arrowleft
    
    * json db setup

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
-import './App.css';
-import Header from './components/Header';
-import Body from './components/Body';
-import Footer from './components/Footer';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import "./App.css";
+import Header from "./components/Header";
+import NotesPage from "./pages/NotesPage";
+import NotePage from "./pages/NotePage";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Body />
-      <Footer />
-    </div>
+    <BrowserRouter>
+      <div className="container dark">
+        <div className="app">
+        <Header />
+        <Routes>
+          <Route path="/" element={<NotesPage />} />
+          <Route path="/note/:id" element={<NotePage />} />
+        </Routes>
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/NotePage.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+import notes from "../assets/data";
+import { ReactComponent as ArrowLeft } from '../assets/arrow-left.svg';
+import { Link } from "react-router-dom";
+
+function NotePage() {
+  let { id } = useParams();
+
+  let note = notes.find((note) => note.id == id);
+
+  return (
+    <div className="note">
+
+        <div className="note-header">
+          <h3>
+            <Link to={"/"}>
+              <ArrowLeft />
+            </Link>
+          </h3>
+        </div>
+
+        <textarea value={note?.body}></textarea>
+    </div>
+  );
+}
+
+export default NotePage;
