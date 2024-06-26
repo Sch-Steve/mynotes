2554f261f4e5cf91e4a7970477488843b70ba622
Sch-Steve <steveschoder@gmail.com>
2024-06-26 16:36:17 +0200

    Dynamic content from json database using useState and useEffect hook in NotePage.js

--- src/pages/NotePage.js
+++ src/pages/NotePage.js
@@ -1,13 +1,23 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import { useParams } from "react-router-dom";
-import notes from "../assets/data";
+//import notes from "../assets/data";
 import { ReactComponent as ArrowLeft } from '../assets/arrow-left.svg';
 import { Link } from "react-router-dom";
 
 function NotePage() {
   let { id } = useParams();
 
-  let note = notes.find((note) => note.id == id);
+  let [note, setNote] = useState(null)
+
+  useEffect(() => {
+    getNote()
+  }, [{id}])
+  //let note = notes.find((note) => note.id == id);
+  let getNote = async () => {
+    let response = await fetch(`http://127.0.0.1:5000/notes/${id}`)
+    let data = await response.json()
+    setNote(data)
+  }
 
   return (
     <div className="note">
